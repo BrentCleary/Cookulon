f0f58a86881d77e6d51a8abb39f7bb0e25646448
Brent Cleary <brent.cleary@gmail.com>
2024-04-13 15:59:36 -0700

    Recipe Ideas Page Constructed with Conditional for Loading + RecipeIdeas Model Built

---
+++ Shared/RecipeIdeas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookulon.Shared
+{
+    public class Idea
+    {
+        public int index { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
