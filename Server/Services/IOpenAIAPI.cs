c995acf33d6f63007a6368410b129c99f7581280
Brent Cleary <brent.cleary@gmail.com>
2024-04-15 06:00:42 -0700

    ChatGPT Models Created

---
+++ Server/Services/IOpenAIAPI.cs
@@ -0,0 +1,10 @@
+﻿using Cookulon.Shared;
+
+namespace Cookulon.Server.Services
+{
+    public interface IOpenAIAPI
+    {
+        Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients);
+        
+    }
+}
---
+++ Server/Services/OpenAIService.cs
@@ -0,0 +1,12 @@
+﻿using Cookulon.Shared;
+
+namespace Cookulon.Server.Services
+{
+    public class OpenAIService : IOpenAIAPI
+    {
+        public Task<List<Idea>> CreateRecipeIdeas(string mealtime, List<string> ingredients)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
